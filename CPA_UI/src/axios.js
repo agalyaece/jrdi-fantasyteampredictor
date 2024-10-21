848adb0e3c0077641ab2b72920055e499feeebd3
agalyaece <agalyapytest@gmail.com>
2024-10-21 13:31:25 +0530

    initial commit

--- CPA_UI/src/axios.js
+++ CPA_UI/src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
 
-    baseURL: "http://localhost:3000"
-    // baseURL: "https://t-20-backend-3.onrender.com"
+    // baseURL: "http://localhost:3000"
+    baseURL: "https://cpa-webservice.onrender.com"
 });
 
 export default instance;
